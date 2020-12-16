fff9a8b60761f40004298e4976293435065e5d9c
AndriiSorokin <68332371+AndriiSorokin@users.noreply.github.com>
2020-12-16 13:08:35 +0200

    переделал таск 5

--- task5.js
+++ task5.js
@@ -1,3 +1,31 @@
+// const products = [
+//   { name: 'Радар', price: 1300, quantity: 4 },
+//   { name: 'Сканер', price: 2700, quantity: 3 },
+//   { name: 'Дроид', price: 400, quantity: 7 },
+//   { name: 'Захват', price: 1200, quantity: 2 },
+// ];
+
+// const getAllPropValues = function (arr, prop) {
+//   let array = []
+
+//   for (let keys of arr) {
+//     for (let key in keys) {
+//       if (key === prop) {
+//         array.push(keys[key])
+//       }
+//     }
+//   }
+// return array
+//   };
+
+// console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+// console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+
+// console.log(getAllPropValues(products, 'category')); // []
+
+
+
 const products = [
   { name: 'Радар', price: 1300, quantity: 4 },
   { name: 'Сканер', price: 2700, quantity: 3 },
@@ -6,19 +34,14 @@ const products = [
 ];
 
 const getAllPropValues = function (arr, prop) {
-  let array = []
-
-  for (let keys of arr) {
-    for (let key in keys) {
-      if (key === prop) {
-        array.push(keys[key])
-      }
+  const array = []
+  for (const key of arr) {
+     if (key.hasOwnProperty([prop])) {
+      array.push(key[prop])
     }
-  }
-return array
-  };
+  } return array
 
-
+}
 
 console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
 
